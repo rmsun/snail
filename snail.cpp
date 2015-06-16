fb4bac81f4ff2581347d6ae30e2c8c88afc929a2
rms.sun <huasong1986@163.com>
2015-06-16 23:34:07 +0800

    add multi-thread

--- snail.cpp
+++ snail.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "Downloader.h"
+#include "Thread.h"
 
 int main( int argc, char** argv )
 {
@@ -14,8 +15,25 @@ int main( int argc, char** argv )
 	Downloader downLoader;
 	std::string url( argv[1] );
 	downLoader.init( url );
-	std::cout << "file's size is: " << downLoader.getFileSize() << std::endl;
-	downLoader.start();
+	int fileSize = 0;
+	fileSize = downLoader.getFileSize();
+	std::cout << "file's size is: " << fileSize << std::endl;
+	if ( 0 == fileSize )
+	{
+		std::cout << "file's size is 0, error!!!" << std::endl;
+		exit( 1 );
+	}
+	
+	//int threadNumber = 2;
+	Thread* thread1 = new Thread();
+	Thread* thread2 = new Thread();
+	thread1->init( boost::bind( &Downloader::start, &downLoader, 0, 0,  fileSize / 2) );
+	thread2->init( boost::bind( &Downloader::start, &downLoader, 1, fileSize / 2 + 1, fileSize ) );
+	thread1->run();
+	thread2->run();
+	thread1->join();
+	thread2->join();
+	//downLoader.start();
 
 	return 0;
 }
--- testThread.cpp
+++ testThread.cpp
@@ -8,12 +8,27 @@ void add( int i, int j )
 	std::cout << i << " + " << j << " = " << i + j << std::endl;
 }
 
+class Calc
+{
+public:
+	Calc(){};
+	~Calc(){};
+public:
+	void add( int i, int j )
+	{
+		std::cout << i << " + " << j << " = " << i + j << std::endl;
+	};
+};
+
 int main( int argc, char** argv )
 {
+	Calc testCalc;
 	Thread* thread1 = new Thread();
 	Thread* thread2 = new Thread();
-	thread1->init( boost::bind( &add, 100, 100 ) );
-	thread2->init( boost::bind( &add, 200, 200 ) );
+	//thread1->init( boost::bind( &add, 100, 100 ) );
+	//thread2->init( boost::bind( &add, 200, 200 ) );
+	thread1->init( boost::bind( &Calc::add, &testCalc, 100, 200 ) );
+	thread2->init( boost::bind( &Calc::add, &testCalc, 200, 200 ) );
 	thread1->run();
 	thread2->run();
 	thread1->join();
